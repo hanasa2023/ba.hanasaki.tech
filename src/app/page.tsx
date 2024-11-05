f069e2bb117758235b6332ec3ddaddc24e9183d2
hanasaki <hanasakayui2022@gmail.com>
2024-11-05 23:36:52 +0800

    🚀 Started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,65 @@
+import {AppBar, Avatar, IconButton, Toolbar, Typography} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    return (
+        <div className="h-screen">
+            <AppBar className="bg-pink-500">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{mr: 2}}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                        BA Tools Resource
+                    </Typography>
+                    <Avatar alt="arona" src="avatar.png"></Avatar>
+                </Toolbar>
+            </AppBar>
+            <main className="flex h-full w-full justify-center items-center">
+                <div className="flex flex-col w-full items-center">
+                    <Image src="/logo.png" alt="logo" width={512} height={512}/>
+                    <div className="flex flex-col w-full items-center">
+                        <h1 className="text-4xl flex">欢迎使用Ba Tools Resource</h1>
+                        <p className="text-xl py-2">
+                            此项目使用
+                            <a href="https://github.com/SchaleDB/SchaleDB">Schale DB</a>
+                            作为数据源，并在此基础上新增了学生的live 2d图像
+                        </p>
+                        <div className="flex flex-col max-w-[1200px] items-start">
+                            <h2 className="text-2xl py-2">✨ 使用方法</h2>
+                            <div className="use-method">
+                                <ul className="list-disc pl-5">
+                                    <li className="mb-2 text-lg p-2">
+                                        此 api 的 url 路径与 Schale DB 的路径基本相同，如果你想访问 Schale DB 中
+                                        <code
+                                            className="bg-gray-200 p-1 mx-1 rounded">data/zh/students.json</code> 的数据，<br/>
+                                        那么你可以通过
+                                        <code className="bg-gray-200 p-1 mx-1 rounded">GET</code>
+                                        <a href="https://api.hanasaki.tech/data/zh/students.json"
+                                        >https://api.hanasaki.tech/data/zh/students.json</a
+                                        >
+                                        来访问该数据
+                                    </li>
+                                    <li>
+                                        新增 live 2D image 的请求路径为
+                                        <code>images/student/l2d/&lt;学生id&gt;.webp</code> <br/>例如：
+                                        <a href="https://api.hanasaki.tech/images/student/l2d/10000.webp"
+                                        >https://api.hanasaki.tech/images/student/l2d/10000.webp</a
+                                        >
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    )
 }
